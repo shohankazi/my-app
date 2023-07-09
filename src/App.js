a3ba98c8f30214d98ffc27788d1519751787db86
shohankazi <shohankazi012@gmail.com>
2023-07-09 19:49:43 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Shohan kazi.
+          Learn React with SHohan Kazi.
         </a>
       </header>
     </div>
