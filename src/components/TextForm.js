08f7603040ae084d620ec454697635908e4b8c64
shohankazi <shohankazi012@gmail.com>
2023-07-12 11:10:30 +0600

    state,event  concept cleared

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,10 @@
-/* 
+/*
     What i also learned today from this video are as written below:
 1. State=condition of any component for eg. textarea component might be empty sometime , sometimes it has letters .
 2. Hooks= A method which helps us to use features of classes in function based component this is because developer like us prefer to use function over classes.
 3. Hooks has this part ---->   const [count,setCount)=useState("Hello Universe"); 
 Here in above hook the value of count is Hello Universe . count is array variable so that we just can't update or change its value like normal variable
+
 */
 import React, { useState } from 'react'
 
