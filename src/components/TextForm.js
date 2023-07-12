86011d79bc05acd4bbf04e281795d2834c4d8f08
shohankazi <shohankazi012@gmail.com>
2023-07-12 11:36:35 +0600

    Added more logic to TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,17 +13,31 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleDownClick = () =>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event) =>{
         setText(event.target.value);
     }
     const [text,setText] = useState("Enter text here 2")
     return (
+        <>
     <div className="container">
         <h1>{props.heading}</h1>
         <div className="form-group mb-3">
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary" onClick={handleDownClick}>Convert to LowerCase</button>
     </div>
+        <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+        </div>
+    </>
   )
 }
