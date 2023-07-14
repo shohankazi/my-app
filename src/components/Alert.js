d222c63d0490ed864b9ef6efc41256f49ae30a65
shohankazi <shohankazi012@gmail.com>
2023-07-14 23:40:06 +0600

    adding & auto dismissing alert messages added

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 function Alert(props) {
+  const capitalize = (word)=>{
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  }
   return (
-    <div className="alert alert-warning alert-dismissible fade show" role="alert">
-  {props.alert}
-  <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+  <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
 </div>
   )
 }
