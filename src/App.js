25cab6867a2bfdd6674b69045e55b43a0c6598de
shohankazi <shohankazi012@gmail.com>
2023-07-15 09:46:10 +0600

    changing title dynamically & adding favicons to textUtils

--- src/App.js
+++ src/App.js
@@ -22,11 +22,14 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled","success");
+      document.title = 'TextUtils - Dark Mode'
+      
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled","success");
+      document.title = 'TextUtils - Light Mode'
     }
   }
   return (
